38610f2f286f0e5c4d952a4d27c058a7cd2b0f94
Souleymane Dia <sould32@vt.edu>
2018-02-18 04:15:35 -0500

    profile template

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
-import { Component } from '@angular/core';
-
+import {UserDataProvider} from '../../providers/user-data/user-data';
 
 @Component({
     selector: 'page-contact',
@@ -12,8 +10,28 @@ import { Component } from '@angular/core';
 
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  user_data = null;
 
+  constructor(public navCtrl: NavController, user_data_provider: UserDataProvider) {
+  	this.user_data = user_data_provider.get_user_data();
+  	console.log(this.user_data)
   }
+
+  public get_fed_tax(){
+  	return (this.user_data.tax.fed).toFixed(2)
+  }
+
+  public get_state_tax(){
+  	return (this.user_data.tax.state).toFixed(2);
+  }
+
+  public get_local_tax(){
+  	return ((this.user_data.ret.user.yearly_income * 5) / 100).toFixed(2);
+  }
+
+  public get_after_tax(){
+  	return (this.user_data.ret.user.yearly_income - (this.user_data.tax.fed + this.user_data.tax.state + Number(this.get_local_tax()) )).toFixed(2);
+  }
+
 
 }
