aa0f81fdfd90da864d94f20f25a5b7a2a894f25b
Andrew Maczugowski <amaczugowski@gmail.com>
2018-02-18 03:53:19 -0500

    working pie chart

--- src/components/my-chart/my-chart.ts
+++ src/components/my-chart/my-chart.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as Chart from 'chart.js';
+import { UserDataProvider } from '../../providers/user-data/user-data';
 
 /**
  * Generated class for the MyChartComponent component.
@@ -15,6 +16,24 @@ export class MyChartComponent implements AfterViewInit {
 
   canvas: any;
   ctx: any;
+  taxes: number;
+  housing: number;
+  transportation: number;
+  spending: number;
+  saving: number;
+
+  constructor(userDataProvider: UserDataProvider) {
+    this.taxes = userDataProvider.getTaxes();
+    this.housing = userDataProvider.getHousing();
+    this.transportation = userDataProvider.getTransportation();
+    this.spending = userDataProvider.getSpending();
+    this.saving = userDataProvider.getSaving();
+    console.log(this.taxes);
+    console.log(this.housing);
+    console.log(this.transportation);
+    console.log(this.spending);
+    console.log(this.saving);
+  }
 
   ngAfterViewInit() {
     this.canvas = document.getElementById('myChart');
@@ -22,14 +41,28 @@ export class MyChartComponent implements AfterViewInit {
     let myChart = new Chart(this.ctx, {
       type: 'pie',
       data: {
-          labels: ["New", "In Progress", "On Hold"],
+          labels: [
+            "Taxes", 
+            "Housing", 
+            "Transportation", 
+            "Spending", 
+            "Saving"
+          ],
           datasets: [{
               label: '# of Votes',
-              data: [1,2,3],
+              data: [
+                this.taxes, 
+                this.housing, 
+                this.transportation, 
+                this.spending, 
+                this.saving
+              ],
               backgroundColor: [
                   'rgba(255, 99, 132, 1)',
                   'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)'
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(135, 84, 250, 1)',
+                  'rgba(22, 128, 44, 1)'
               ],
               borderWidth: 1
           }]
@@ -39,7 +72,6 @@ export class MyChartComponent implements AfterViewInit {
         display:true
       }
     });
-    console.log('done');
   }
 
 }
--- src/providers/user-data/user-data.ts
+++ src/providers/user-data/user-data.ts
@@ -19,6 +19,34 @@ export class UserDataProvider {
     return this.user_data;
   }
 
+  public getTaxes() {
+    return this.user_data.tax.fed + this.user_data.tax.state;
+  }
+
+  public getHousing() {
+    return this.user_data.ret.user.house.morgage + 
+        this.user_data.ret.user.house.house_maintenance + 
+        this.user_data.ret.user.house.utulity;
+  }
+
+  public getTransportation() {
+    return this.user_data.ret.user.vehicule.car_payment + 
+        this.user_data.ret.user.vehicule.fuel;
+  }
+
+  public getSpending() {
+    return this.user_data.ret.user.spending.grocery +
+        this.user_data.ret.user.spending.monthly_suscription +
+        this.user_data.ret.user.spending.restaurant +
+        this.user_data.ret.user.spending.other_spending;
+  }
+
+  public getSaving() {
+    return this.user_data.ret.user.saving.investment +
+        this.user_data.ret.user.saving.saving_account +
+        this.user_data.ret.user.saving.emrgency_saving;
+  }
+
   load() {
     return new Promise((resolve, reject) => {
       this.http
